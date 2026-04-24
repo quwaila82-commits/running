6051e2b53469ec2a3c621d6bee2fc9cf45a2ff87
quwaila82-commits <quwaila82@gmail.com>
2026-04-24 10:12:07 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='hm-v9';
-const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+const CACHE='hm27-v1';
+const A=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{if(e.request.method==='GET'&&resp.ok){const c=resp.clone();caches.open(CACHE).then(ch=>ch.put(e.request,c))}return resp}).catch(()=>caches.match('./index.html'))))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{if(e.request.method==='GET'&&resp.ok){const c=resp.clone();caches.open(CACHE).then(ca=>ca.put(e.request,c))}return resp}).catch(()=>caches.match('./index.html'))))});
